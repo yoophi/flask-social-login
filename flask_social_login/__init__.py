2bc8aebac42e603863b17c274a521a777c1d5ab9
Pyunghyuk Yoo <yoophi@gmail.com>
2021-01-29 19:53:33 +0900

    Bump version: 0.1.0 → 0.2.0

--- flask_social_login/__init__.py
+++ flask_social_login/__init__.py
@@ -6,7 +6,7 @@
     :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "0.1.0"
+__version__ = "__version__ = '0.2.0'"
 
 from .core import Social
 from .datastore import SQLAlchemyConnectionDatastore
